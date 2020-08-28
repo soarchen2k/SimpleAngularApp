c810b1502150e1cc44ebcdbb0a565c4366e335fb
soarchen <chenchong.blog@gmail.com>
2020-08-28 12:00:02 -0400

    1.0.4 使用 @Input decorator  使父类 html 可以访问子类的 class 中的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FooterComponent} from './footer/footer.component';
 
 @Component({
@@ -6,19 +6,29 @@ import {FooterComponent} from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+// 实现 OnInit 接口
+export class AppComponent implements OnInit {
   title = 'SimpleAngularApp';
 
   // 使用 @ViewChild Decorator，连接 footer 中的 #footer 选择器
   // 与 footerComponent 变量
   @ViewChild('footer', {static: true})
-  // 定义一个 footerComponent 变量，变量类型为 FooterComponent
+    // 定义一个 footerComponent 变量，变量类型为 FooterComponent
   footerComponent: FooterComponent;
+
+  // 声明一个 string 类型的变量，在 TypeScript 中，用小写的 string 来声明字符串变量
+  startTime: string;
 
   // tslint:disable-next-line:typedef
   updateLastAccessed() {
     console.log('The previous last accessed value was ' + this.footerComponent.lastAccessed);
     // 使用 footerComponent 变量来访问 FooterComponent 中的参数
     this.footerComponent.lastAccessed = new Date().toString();
+  }
+
+  // 当 component 启动时，初始化一个 startTime 字符串
+  ngOnInit(): void {
+    this.startTime = new Date().toString();
   }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  // @Input() decorator 无需任何参数，
+  // 但如果需要声明不同的变量名，则可以使用如 @Input('lastAc') 的格式，
+  // 并在需要引用这个变量的地方使用相同的名称
+  @Input()
   lastAccessed = '';
 
   constructor() { }
