8f778615653e71eccd8a875df462fe6696bd8dc2
soarchen <chenchong.blog@gmail.com>
2020-08-28 11:16:33 -0400

    1.0.3 使用 @ViewChild decoration 连接两个 class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'SimpleAngularApp';
 
+  // 使用 @ViewChild Decorator，连接 footer 中的 #footer 选择器
+  // 与 footerComponent 变量
+  @ViewChild('footer', {static: true})
+  // 定义一个 footerComponent 变量，变量类型为 FooterComponent
+  footerComponent: FooterComponent;
+
   // tslint:disable-next-line:typedef
   updateLastAccessed() {
-    console.log('Button was clicked on: ' + new Date());
+    console.log('The previous last accessed value was ' + this.footerComponent.lastAccessed);
+    // 使用 footerComponent 变量来访问 FooterComponent 中的参数
+    this.footerComponent.lastAccessed = new Date().toString();
   }
 }
